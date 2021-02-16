71e33d7d2955029f3ce1f185998a0675e48df052
Raúl Morales <raulmdaw@gmail.com>
2021-02-16 18:56:11 +0100

    rutas entryModule separadas en fichero de rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AthleteDataRegisterComponent } from './entry/athlete-data-register/athlete-data-register.component';
-import { LogInComponent } from './entry/log-in/log-in.component';
-import { SignUpComponent } from './entry/sign-up/sign-up.component';
-import { WelcomeComponent } from './entry/welcome/welcome.component';
 
-const routes: Routes = [
-  {
-    path: 'welcome', component: WelcomeComponent
-  },
-  {
-    path: 'login', component: LogInComponent
-  },
-  {
-    path: 'signup', component: SignUpComponent
-  },
-  {
-    path: 'athlete', component: AthleteDataRegisterComponent
-  },
-];
+// Módulos
+import { EntryRoutingModule } from './entry/entry.routing';
+
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EntryRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
