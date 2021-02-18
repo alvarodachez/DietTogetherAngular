d31f36269d2f2ef40a33a5218ee88b3ca63b4265
Raúl Morales <raulmdaw@gmail.com>
2021-02-18 19:18:47 +0100

    formulario atleta mejorado

--- src/app/entry/models/athlete.class.ts
+++ src/app/entry/models/athlete.class.ts
@@ -2,7 +2,9 @@ export class AthleteClass {
   constructor (
       public name: string,
       public surname: string,
-      public weight: number,
-      public height: number,
+      public weightKilograms: string,
+      public weightGrams: string,
+      public heightMeters: string,
+      public heightCentimeters: string,
       public birthDay?: Date) {}
 }
