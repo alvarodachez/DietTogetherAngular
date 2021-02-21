67731557fd78a56683d7ed0ecebdcb5f338797e8
Raúl Morales <raulmdaw@gmail.com>
2021-02-21 20:08:06 +0100

    se agregan estilos scss y se modifica diseño de pantallas login y atleta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntryModule } from './entry/entry.module';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
