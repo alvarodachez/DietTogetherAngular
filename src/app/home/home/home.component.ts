727d2ae95c0db85c8640633b43d92341ff5fc462
Alvaro <sanchez99esp@gmail.com>
2021-03-10 13:31:44 +0100

    añadido mensaje al entrar en el login para mostrar la version alpha

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +13,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+
 
 }
