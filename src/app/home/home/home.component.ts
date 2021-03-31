42daf3936087865993c1ab9359d23161b78e6892
Raúl Morales <raulmdaw@gmail.com>
2021-03-31 15:33:39 +0200

    Cambios provisionales en home para mostrar dieta del día

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Observable, Subject } from 'rxjs';
 import { LogInService } from '../../entry/services/log-in.service';
+import { RegimeService } from 'src/app/regime/services/regime.service';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,28 @@ import { LogInService } from '../../entry/services/log-in.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor( private login:LogInService) { }
+  actualUser: string;
+  dateNow: number;
+
+  constructor(private login:LogInService, private regimeService: RegimeService) { }
 
   ngOnInit(): void {
     this.login.isUserInSession();
+
+    this.getActualUser();
+
+    this.getDateNow();
   }
 
+  getActualUser() {
+    this.actualUser = localStorage.getItem("dietUsernameSession");
+  }
+
+  getDateNow() {
+    this.dateNow = Date.now();
+  }
+
+  
 
 
 
